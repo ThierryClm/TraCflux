220fbfa2fc5899f78ce2751ab4b0047b8d864486
thierryclm <251635330+ThierryClm@users.noreply.github.com>
2026-08-26 14:03:37 +0200

    Release v1.2.2

--- src/version.js
+++ src/version.js
@@ -2,6 +2,6 @@
  * Application version.
  * Update this file when releasing a new version.
  */
-export const APP_VERSION = '1.2.1';
+export const APP_VERSION = '1.2.2';
 export const APP_NAME = 'TraCflux';
 export const APP_DESCRIPTION = 'Outil de conception et d\'optimisation de diagrammes de feux tricolores';
